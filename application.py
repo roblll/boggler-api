ac9048b74c85d4b4332b2e3ef63281154991204c
rob <lenon.rob@gmail.com>
2021-11-15 16:21:47 -0600

    Update application.py add api route and return words found json

--- application.py
+++ application.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return 'hi'
+    return 'test'
+
+@application.route('/api', methods=['POST'])
+def hello():
+    
+    words_found = {"words": ["apple", "ball", "cat", "dog"]}
+    
+    return jsonify(words_found)
