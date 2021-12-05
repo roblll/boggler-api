17d5e2e7de1c4ee12af2e8e20ccfaac4d558bae0
rob <lenon.rob@gmail.com>
2021-12-05 14:11:48 -0600

    Add /words/Q.py

---
+++ words/Q.py
@@ -0,0 +1,22 @@
+Q = [
+    # 3 letters
+    'qat',
+    'qis',
+    'qua',
+    # 4 letters
+    'qadi',
+    'qaid',
+    'qats',
+    'qoph',
+    'quad',
+    'quag',
+    'quai',
+    'quay',
+    'quey',
+    'quid',
+    'quin',
+    'quip',
+    'quit',
+    'quiz',
+    'quod',
+]
