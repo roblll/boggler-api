c960dd2f4136f08752715c966f3d072e071f7996
rob <lenon.rob@gmail.com>
2021-12-12 20:36:51 -0600

    Update application.py to import A to Z from words

--- application.py
+++ application.py
@@ -1,6 +1,33 @@
 from flask import Flask, jsonify
 application = Flask(__name__)
 
+from words.A import A
+from words.B import B
+from words.C import C
+from words.D import D
+from words.E import E
+from words.F import F
+from words.G import G
+from words.H import H
+from words.I import I
+from words.J import J
+from words.K import K
+from words.L import L
+from words.M import M
+from words.N import N
+from words.O import O
+from words.P import P
+from words.Q import Q
+from words.R import R
+from words.S import S
+from words.T import T
+from words.U import U
+from words.V import V
+from words.W import W
+from words.X import X
+from words.Y import Y
+from words.Z import Z
+
 @application.route('/test')
 def test():
     return 'test'
